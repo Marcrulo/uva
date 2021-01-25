1d71300932c899298b384714a50cc26dd52a5b45
Marcrulo <marcus@presutti.eu>
2021-01-25 23:12:36 +0100

    update

--- introduction/getting_started/medium/10141.cpp
+++ introduction/getting_started/medium/10141.cpp
@@ -1,3 +1,10 @@
+/*
+
+I give up for now. I realize, that I have misunderstood the assignment, and will not proceed any further.
+
+*/
+
+
 #include <iostream>
 #include <vector>
 #include <string>
@@ -11,33 +18,62 @@ int main()
     freopen("/home/marc8165/Documents/git-repos/uva/testI.txt", "r", stdin);
 	//freopen("/home/marc8165/Documents/git-repos/uva/testO.txt", "w", stdout);
 
-    int N, P;
+    int N, P, num=1;
     while (cin >> N >> P){
+        if(!N){
+            break;
+        }
         string blank;
         for (int i = 0; i < N+1; i++)
         {
             getline(cin, blank, '\n');
+            //cin >> blank;
         }
-        
         string names[P];
         double prices[P];
         int reqs[P];
         for (int i = 0; i < P; i++){
-            cin >> names[i]; 
+            cin >> names[i];
             cin >> prices[i];
             cin >> reqs[i];
-            for (int j = 0; j < reqs[i]; j++)
+            for (int j = 0; j < reqs[i]+1; j++)
             {
                 getline(cin, blank, '\n');
+                //cin >> blank;
             }
-            
-            
-            
+                 
         }
-        cout << *max_element(prices, prices+P);
+        int index = distance(reqs, max_element(reqs, reqs+P));
+        int max_req = reqs[index];
+
+        vector<int> multiple;
+        for (int i = 0; i < P; i++)
+        {
+            if(reqs[i] == max_req){
+                multiple.push_back(i);
+            }
+        }
+        
+        double min_price = INT_MAX;
+        for (int i = 0; i < multiple.size(); i++)
+        {
+            if(prices[multiple[i]] < min_price){
+                
+                min_price = prices[multiple[i]];
+                index = multiple[i];
+                //cout << "index: " << index << endl;
+            }
+        }
+        //cout << "Latest index: " << index << endl;
         
 
 
+        if(num > 1){
+            cout << endl;
+        }
+        cout << "RFP #" << num << endl;
+        cout << names[index] << endl;  
+        num++;
     }
     
 
