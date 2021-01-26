4a23ba610a3df7df940c2d06b7d29b9428f3cfd2
Marcrulo <marcus@presutti.eu>
2021-01-26 20:45:12 +0100

    update

--- introduction/getting_started/medium/10141.cpp
+++ introduction/getting_started/medium/10141.cpp
@@ -11,51 +11,57 @@ using namespace std;
 
 int main()
 {
-    freopen("/home/marc8165/Documents/git-repos/uva/testI.txt", "r", stdin);
+    //freopen("/home/marc8165/Documents/git-repos/uva/testI.txt", "r", stdin);
 	//freopen("/home/marc8165/Documents/git-repos/uva/testO.txt", "w", stdout);
 
-    int N, P;
-    while (cin >> N >> P){
-        cin.ignore(1);
-        if(!N){
-            break;
-        }
-        vector<string> requirements;
-        for (int i = 0; i < N; i++)
-        {
-            string a;
-            
-            getline(cin, a); 
-            requirements.push_back(a);
-        }
-        string names[P];
-        double prices[P];
-        double compliances[P];
+    int N,P,R,num=1;
+    double price,bestCompliance,bestPrice;
+    string name, bestName;
+;
 
+    string placeholder;
+    while (scanf("%d %d\n", &N, &P), N || P){
+        cin.ignore(1);
+
+        for (int i = 0; i < N; i++){
+            getline(cin,placeholder);
+        }
+        bestCompliance = 0.0;
+        bestPrice = DBL_MAX;
         for (int i = 0; i < P; i++)
         {
-            cin >> names[i] >> prices[i];
-            int features;
-            cin >> features;
-            cin.ignore(1);
-            double compliance = 0;
-            for (int j = 0; j < features; j++)
-            {
-                string feat;
-                getline(cin, feat, '\n');
+            getline(cin, name);
+            scanf("%lf %d\n", &price, &R);
 
-                if(find(requirements.begin(), requirements.end(), feat) != requirements.end()){
-                    compliance += 1;
+            
+            if(R > bestCompliance){
+                bestCompliance = R;
+                bestPrice = price;
+                bestName = name;
+            }
+            else if(R == bestCompliance){
+                if(price < bestPrice){
+                    bestPrice = price;
+                    bestName = name;
                 }
             }
-            compliances[i] = compliance/features;
+
+            for (int i = 0; i < R; i++){
+                getline(cin,placeholder);
+            }
         }
+        if(num > 1){
+            cout << endl;
+        }
+        cout << "RFP #" << num << endl;
+        cout << bestName << endl;
+        
+        num++;
 
-        //vector<int> maxes;
 
         
         
-        
+
 
 
     }
